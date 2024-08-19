459aa0880d6e6487bf2af2e57016e52c4da62a1b
fmacedosantos <felipemacedo986@gmail.com>
2024-08-19 09:16:39 -0300

    Utilizando o useState

--- App.tsx
+++ App.tsx
@@ -1,10 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [quantity, setQuantity] = useState<number>(1)
+  const [price, setPrice] = useState<number>(10.9)
+  const basePrice = 10.9
+
+  const addNumber = () => {
+    setQuantity((prevQuantity) => {
+      const newQuantity = prevQuantity + 1
+      setPrice(basePrice * newQuantity)
+      return newQuantity
+    })
+  }
+
+  const removeNumber = () => {
+    setQuantity((prevQuantity) => {
+      const newQuantity = prevQuantity - 1
+      setPrice(basePrice * newQuantity)
+      return newQuantity
+    })
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <View style={styles.buttonRow}>
+        <Button title='-' onPress={removeNumber}/>
+        <Text style={styles.textLabel}>{quantity}</Text>
+        <Button title='+' onPress={addNumber}/>
+      </View>
+      <Text>R${price}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +42,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  textLabel: {
+    fontSize: 22,
+    marginHorizontal: 10,
+  },
+  buttonRow: {
+    flexDirection: 'row',
   },
 });
