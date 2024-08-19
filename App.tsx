00ac2eb1e6bdc5b522660d24977c12069c1c5ecf
fmacedosantos <felipemacedo986@gmail.com>
2024-08-19 09:52:38 -0300

    Utilizando o useReducer junto com o useRef

--- App.tsx
+++ App.tsx
@@ -1,22 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import { useRef } from 'react';
+import { useReducer, useRef } from 'react';
 import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
+const ActionsTypes = {
+  RESET: '  RESET',
+  WRITE: 'WRITE'
+}
+
+const reducer = (state: any, action: any) => {
+  switch(action.type){
+    case ActionsTypes.RESET:
+      state.textInputRef.current.setNativeProps({ text: "" })
+      return state
+
+    case ActionsTypes.WRITE:
+      state.textInputRef.current.setNativeProps({ text: "Escreva seu nome!" })
+      return state
+  }
+}
+
 export default function App() {
-  const textInputRef = useRef<TextInput>(null)
+  const initialState = {
+    textInputRef: useRef<TextInput>(null)
+  }
+
+  const [state, dispatch] = useReducer(reducer, initialState)
 
   const resetButton = () => {
-    if(textInputRef.current){
-      textInputRef.current.setNativeProps({
-        text: ""
-      })
-    }
+    dispatch({type: ActionsTypes.RESET})
+  }
+
+  const writeButton = () => {
+    dispatch({type: ActionsTypes.WRITE})
   }
 
   return (
     <View style={styles.container}>
-      <TextInput style={styles.input} ref={textInputRef}/>
+      <TextInput style={styles.input} ref={state.textInputRef}/>
       <Button title='RESETAR' onPress={resetButton}/>
+      <Button title='INSTRUÇÃO' onPress={writeButton} />
       <StatusBar style="auto" />
     </View>
   );
